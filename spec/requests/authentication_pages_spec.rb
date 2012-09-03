49d9c4ba42a1c32c184b9f64ce1eb925042cff29
davidboon <davidboon786@gmail.com>
2012-09-03 16:57:13 +0530

    check this

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,8 +21,9 @@ describe "Authentication" do
     end
 
   describe "with valid information" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { valid_signin(user) }
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+      
       before do
         fill_in "Email",    with: user.email
         fill_in "Password", with: user.password
@@ -30,7 +31,8 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
